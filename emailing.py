f436dda75d01cfacf223da627cb3e95ee4d14b56
rajasreegorrepati24@gmail.com <rajasreegorrepati24@gmail.com>
2023-10-10 12:06:15 +0530

    create a webcam that detects object and send email with the attachment of object

--- emailing.py
+++ emailing.py
@@ -1,2 +1,26 @@
-def send_email():
-    print("email was sent")
+import imghdr
+import smtplib
+from email.message import EmailMessage
+
+PASSWORD="hbjjvwezruubofhd"
+SENDER="rajasreegorrepati24@gmail.com"
+RECEIVER="rajasreegorrepati24@gmail.com"
+def send_email(image_path):
+    print("send email function started")
+    email_message=EmailMessage()
+    email_message["Subject"]= "New customer showed up!"
+    email_message.set_content=("Hey, we just saw a new customer!")
+
+    with open(image_path,"rb") as file:
+        content=file.read()
+    email_message.add_attachment(content,maintype="image",subtype=imghdr.what(None,content))
+    gmail=smtplib.SMTP("smtp.gmail.com",587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER,PASSWORD)
+    gmail.sendmail(SENDER,RECEIVER,email_message.as_string())
+    gmail.quit()
+    print("send email function ended")
+
+if __name__=="__main__":
+    send_email("images/90.png")
